ba57739bde3ba911a5210e7d561abf14eb1e04fa
Thiago Novato <thiagonovato@gmail.com>
2019-03-04 19:33:01 -0300

    Finalizando o desafio 2 bootcamp.

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -29,7 +29,12 @@ class AvailableController {
       "15:00",
       "16:00",
       "17:00",
-      "18:00"
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00"
     ];
 
     const available = schedule.map(time => {
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const DashboardController = require("./app/controllers/DashboardController");
 const FilesController = require("./app/controllers/FilesController");
 const AppointmentController = require("./app/controllers/AppointmentController");
 const AvailableController = require("./app/controllers/AvailableController");
+const ScheduleController = require("./app/controllers/ScheduleController");
 
 const authMiddleware = require("./app/middlewares/auth");
 const guestMiddleware = require("./app/middlewares/guest");
@@ -28,14 +29,14 @@ routes.get("/signup", guestMiddleware, UserController.create);
 routes.post("/signup", upload.single("avatar"), UserController.store);
 
 routes.use("/app", authMiddleware);
-
 routes.get("/app/logout", SessionController.destroy);
 
 routes.get("/app/dashboard", DashboardController.index);
-
 routes.get("/app/appointments/new/:provider", AppointmentController.create);
 routes.post("/app/appointments/new/:provider", AppointmentController.store);
 
 routes.get("/app/available/:provider", AvailableController.index);
+
+routes.get("/app/schedule", ScheduleController.index);
 
 module.exports = routes;
